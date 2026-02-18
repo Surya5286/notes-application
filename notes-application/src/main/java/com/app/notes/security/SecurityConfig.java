bebe31995d7ee287b0e7074de01f18b4d415d518
surya <annakalyan286@gmail.com>
2026-02-18 13:15:34 +0530

    Multi User - InMemory config changes updated.

--- notes-application/src/main/java/com/app/notes/security/SecurityConfig.java
+++ notes-application/src/main/java/com/app/notes/security/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +16,35 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/demo/**")
-                        .permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated()
                 )
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults()); // or .formLogin(Customizer.withDefaults())
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if(!manager.userExists("user1")) {
+            manager.createUser(
+                            User.withUsername("user1")
+                                    .password("{noop}password1")
+                                    .roles("USER")
+                                    .build()
+            );
+        }
+
+        if(!manager.userExists("admin")) {
+            manager.createUser(
+                            User.withUsername("admin")
+                                    .password("{noop}password")
+                                    .roles("ADMIN")
+                                    .build()
+            );
+        }
+        return manager;
     }
 }
