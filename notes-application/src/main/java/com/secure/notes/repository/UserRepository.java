5b438b48e65d25df6d55b59bbd405b6805b4dc8b
surya <annakalyan286@gmail.com>
2026-02-19 17:42:08 +0530

    Custom User Handling & DB Storing.

---
+++ notes-application/src/main/java/com/secure/notes/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.secure.notes.repository;
+
+import com.secure.notes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+
+    Boolean existsByUserName(String admin);
+}
