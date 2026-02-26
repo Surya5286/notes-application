e54c074ee55be846201d927373da8159cea60086
surya <annakalyan286@gmail.com>
2026-02-26 18:07:49 +0530

    Implement JWT AuthToken Filter.

---
+++ notes-application/src/main/java/com/secure/notes/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,54 @@
+package com.secure.notes.security.jwt;
+
+import com.secure.notes.security.services.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        log.debug("AuthTokenFilter called for URI: {}", request.getRequestURI());
+        try{
+            var jwt = parseJwt(request);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(jwt);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            log.debug("Roles from jwt: {}", authentication.getAuthorities());
+
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } catch (Exception e) {
+            log.error("Cannot set user authentication: {}", e);
+        }
+
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        var jwt = jwtUtils.getJwtFromHeader(request);
+        log.debug("JWT received: {}", jwt);
+        return jwt;
+    }
+}
